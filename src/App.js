f828cfabb0967a5240ae7d92fef8898a5ac74814
Amy <amy.greene029@gmail.com>
2021-10-25 18:27:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Movie from './Components/Movie';
+
+const mainAPI = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ce0c765e9a4c573c83baf64f39367b10&page=1";
+const searchAPI = "https://www.themoviedb.org/3/search/movie?&api_key=ce0c765e9a4c573c83baf64f39367b10&query=";
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(mainAPI)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response.results)
+        setMovies(response.results);
+      });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="movie-container">
+        {movies.length > 0 && movies.map((movie) => 
+          <Movie key={movie.id} {...movie}/>
+        )}
     </div>
   );
 }
