58903ac7084e2bc4f4c091a004fc369a65dbd44b
Amy <amy.greene029@gmail.com>
2021-10-26 12:12:45 +0100

    add search api

--- src/App.js
+++ src/App.js
@@ -2,27 +2,53 @@ import React, {useEffect, useState} from 'react';
 import Movie from './Components/Movie';
 
 const mainAPI = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ce0c765e9a4c573c83baf64f39367b10&page=1";
-const searchAPI = "https://www.themoviedb.org/3/search/movie?&api_key=ce0c765e9a4c573c83baf64f39367b10&query=";
+const searchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=ce0c765e9a4c573c83baf64f39367b10&query=";
 
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     fetch(mainAPI)
       .then((res) => res.json())
       .then((response) => {
-        console.log(response.results)
         setMovies(response.results);
       });
   }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchValue){
+      fetch(searchAPI + searchValue)
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(response)
+          setMovies(response.results);
+        });
+        setSearchValue('')
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchValue(e.target.value);
+
+  }
   
   return (
-    <div className="movie-container">
-        {movies.length > 0 && movies.map((movie) => 
-          <Movie key={movie.id} {...movie}/>
-        )}
-    </div>
+    <>
+      <header>
+      <form onSubmit={handleOnSubmit}>
+        <input className="search" type="search" placeholder="Search..." value={searchValue} onChange={handleOnChange}/>
+      </form>
+      </header>
+      <div className="movie-container"> 
+          {movies.length > 0 && movies.map((movie) => 
+            <Movie key={movie.id} {...movie}/>
+          )}
+      </div>
+    </>
   );
 }
 
